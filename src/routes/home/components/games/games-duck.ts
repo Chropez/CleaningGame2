dd1e376af744a31393da89fac782729165b07275
Christopher Lopez <christopher.lopez@comeon.com>
2019-06-29 23:01:04 +0200

    Fix check game name availability bug

--- src/routes/home/components/games/games-duck.ts
+++ src/routes/home/components/games/games-duck.ts
@@ -1,13 +1,16 @@
-import { AppActionCreator } from 'store';
+import { AppActionCreator, AppThunkDispatch } from 'store';
 import Game from 'models/game';
-import { getId, useAnimals, useAdjectives } from 'animal-id';
+import { getId } from 'animal-id';
+import Firestore from 'typings/firestore';
 
 enum GamesActionTypes {
   CreateGame = 'GAMES_CREATE_GAME',
   SubscribeToGames = 'GAMES_SUBSCRIBE_TO_GAMES',
   UnsubscribeToGames = 'GAMES_UNSUBSCRIBE_TO_GAMES',
   GetAvailableGameName = 'GAMES_GET_AVAILABLE_GAME_NAME',
-  CheckAvailableNameRequest = 'GAMES_CHECK_AVAILABLE_NAME_REQUEST'
+  GameNameCheckRequested = 'GAMES_GAME_NAME_CHECK_REQUESTED',
+  GameNameAvailable = 'GAMES_GAME_NAME_AVAILABLE',
+  GameNameUnavailable = 'GAMES_GAME_NAME_UNAVAILABLE'
 }
 
 export const subscribeToGames: AppActionCreator = () => (
@@ -28,30 +31,29 @@ export const unsubscribeToGames: AppActionCreator = () => (
   { getFirestore }
 ) => getFirestore().unsetListener({ collection: 'games' });
 
-const getAvailableGameName: AppActionCreator = () => async (
-  dispatch,
-  getState,
-  { getFirestore }
-) => {
-  let firestore = getFirestore();
+const getAvailableGameName = async (
+  dispatch: AppThunkDispatch,
+  firestore: Firestore
+): Promise<string> => {
   let gameId = getId();
 
   dispatch({
-    type: GamesActionTypes.CheckAvailableNameRequest,
+    type: GamesActionTypes.GameNameCheckRequested,
     payload: { gameId }
   });
 
-  // let game =
-  await firestore.get({
+  let game = await firestore.get({
     collection: 'games',
     doc: gameId,
     storeAs: 'availableGameName'
   });
 
-  // if (game.exists) {
-  //   return await getAvailableGameName(firestore);
-  // }
+  if (game.exists) {
+    dispatch({ type: GamesActionTypes.GameNameUnavailable });
+    return await getAvailableGameName(dispatch, firestore);
+  }
 
+  dispatch({ type: GamesActionTypes.GameNameAvailable });
   return gameId;
 };
 
@@ -64,9 +66,8 @@ export const createGame: AppActionCreator = () => async (
   let userId = state.firebase.auth.uid;
 
   let firestore = getFirestore();
-  useAdjectives(['annoying', 'annoying-p']);
-  useAnimals(['parrot']);
-  let id = await getAvailableGameName(firestore);
+  let gameId = await getAvailableGameName(dispatch, firestore);
+
   let game: Game = {
     name: 'Game',
     createdAt: firestore.Timestamp.now().toMillis(),
@@ -74,7 +75,10 @@ export const createGame: AppActionCreator = () => async (
     playerIds: [userId]
   };
 
-  dispatch({ type: GamesActionTypes.CreateGame, payload: { ...game, id } });
+  dispatch({
+    type: GamesActionTypes.CreateGame,
+    payload: { ...game, documentId: gameId }
+  });
 
-  // firestore.set({ collection: 'games', doc: getId() }, game);
+  firestore.set({ collection: 'games', doc: getId() }, game);
 };
